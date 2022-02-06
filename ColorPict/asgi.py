8134f70dfec4e0a8cff5980c68cfdd643ff8c6fe
Joel Keller <joeldavidkeller@gmail.com>
2022-02-06 03:47:22 -0900

    Consistent Naming. ~JK

--- ColorPict/asgi.py
+++ ColorPict/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FashionFind project.
+ASGI config for ColorPict project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FashionFind.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ColorPict.settings')
 
 application = get_asgi_application()
--- ColorPict/wsgi.py
+++ ColorPict/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FashionFind project.
+WSGI config for ColorPict project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FashionFind.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ColorPict.settings')
 
 application = get_wsgi_application()
